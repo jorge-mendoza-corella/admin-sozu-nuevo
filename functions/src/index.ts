275e4054d10a2276688c8ff644373cea0eb76c04
Jorge Mendoza-Ubuntu <mencor.corp@gmail.com>
2025-01-27 16:24:54 -0600

    Nueva config

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,3 @@
-import * as functions from 'firebase-functions';
-import { Nuxt } from 'nuxt';
-import * as admin from 'firebase-admin';
+import { handle } from 'nitropack/functions';
 
-// Inicializa Firebase Admin
-admin.initializeApp();
-
-const config = {
-  dev: false, // Para producción
-  buildDir: 'nuxt-dist', // Directorio de salida de la compilación Nuxt.js
-};
-
-// Inicia Nuxt.js
-const nuxt = new Nuxt(config);
-
-export const ssr = functions.https.onRequest(async (req, res) => {
-  await nuxt.ready();
-  nuxt.render(req, res);
-});
+export const ssr = handle();
