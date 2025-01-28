61922a44cd0f8a9ff744585fbeb28bdd463e44c2
Jorge Mendoza-Ubuntu <mencor.corp@gmail.com>
2025-01-27 18:46:41 -0600

    Cambie a node 22 y configure runtime

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
-import { defineNitroConfig } from 'nitropack/functions';
+import { onRequest } from 'firebase-functions/v2/https';
 
-// Define las funciones aquí
-export default defineNitroConfig({
-  // Configuración específica de Nitro
+// Define tu función de Firebase
+export const api = onRequest((req, res) => {
+  res.send({ message: 'Hello from Firebase and Nuxt with Nitro!' });
 });
